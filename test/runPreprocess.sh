01747034b4417540425563066bedf51462d78b7f
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2024-08-16 10:13:27 -0400

    revamped apps-directory and removed test data

--- test/runPreprocess.sh
+++
@@ -1,9 +0,0 @@
-path="/rsrch3/scratch/bcb/jdou1/scAncestry/Monopogen"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${path}/apps
-
-
-python  ${path}/src/Monopogen.py  preProcess -b bam.lst -o out  -a ${path}/apps -t 8
-
-
-
-     
